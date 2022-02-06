82aaab4684d654b5a0da1366b900f45e4a79570e
KAMADA Mayo <mayok32549@gmail.com>
2022-02-06 23:27:25 +0900

    create archive.php

---
+++ archive.php
@@ -0,0 +1,99 @@
+<?php 
+/**
+ * Template Name: archive-menu
+ * Template Post Type: post,
+ */
+get_header(); ?>
+        <main class="l-main c-grid">
+            <div class="c-mainvisual c-mainvisual--archive">
+                <div class="c-mainvisual__headline c-mainvisual__headline--archive">
+                    <h2>Menu:</h2>
+                    <h4>チーズバーガー</h4>
+                </div>
+                <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/mainvisual/archive---tab.jpeg?raw=true" alt="" class="c-mainvisual__sp">
+                <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/mainvisual/archive---tab.jpeg?raw=true" alt="" class="c-mainvisual__tab">
+                <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/mainvisual/archive--pc.png?raw=true" alt="" class="c-mainvisual__pc">
+            </div>
+            <section class="p-article u-width--89 u-mg--lr-auto">
+                <h1>小見出しが入ります</h1>
+                <p class="u-mg--tb30">
+                    テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+                    テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+                    テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+                    テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+                    テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+                    テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+                </p>
+            </section>
+            <figure class="p-post">
+                <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/archive/archive-visual.png?raw=true" alt="">
+                <figcaption>
+                    <div class="p-post__wrapper">
+                        <h2>チーズバーガー</h2>
+                        <h3>小見出しが入ります</h3>
+                        <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+                            テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+                        </p>
+                    </div>
+                    <button class="c-detail">
+                        詳しく見る
+                    </button>
+                </figcaption>
+            </figure>
+            <figure class="p-post">
+                <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/archive/archive-visual.png?raw=true" alt="">
+                <figcaption>
+                    <div class="p-post__wrapper">
+                        <h2>ダブルチーズバーガー</h2>
+                        <h3>小見出しが入ります</h3>
+                        <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+                            テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+                        </p>
+                    </div>
+                    <button class="c-detail">
+                        詳しく見る
+                    </button>
+                </figcaption>
+            </figure>
+            <figure class="p-post">
+                <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/archive/archive-visual.png?raw=true" alt="">
+                <figcaption>
+                    <div class="p-post__wrapper">
+                        <h2>スペシャルチーズバーガー</h2>
+                        <h3>小見出しが入ります</h3>
+                        <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+                            テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+                        </p>
+                    </div>
+                    <button class="c-detail">
+                        詳しく見る
+                    </button>
+                </figcaption>
+            </figure>
+            <div class="p-pagination">
+                <div class="u-visibility__tab">
+                    <p>page</p>
+                    <p>1</p>
+                    <p>10</p>
+                </div>
+                <div class="p-pagination__prev">
+                    <a class="" href=""><<</a>
+                    <a class="u-visibility__sp" href="">前へ</a>
+                </div>
+                <a href="" class="p-pagination__number u-visibility__tab">1</a>
+                <a href="" class="p-pagination__number u-visibility__tab">2</a>
+                <a href="" class="p-pagination__number u-visibility__tab">3</a>
+                <a href="" class="p-pagination__number u-visibility__tab">4</a>
+                <a href="" class="p-pagination__number u-visibility__tab">5</a>
+                <a href="" class="p-pagination__number u-visibility__tab">6</a>
+                <a href="" class="p-pagination__number u-visibility__tab">7</a>
+                <a href="" class="p-pagination__number u-visibility__tab">8</a>
+                <a href="" class="p-pagination__number u-visibility__tab">9</a>
+                <div class="p-pagination__next">
+                    <a class="u-visibility__sp" href="">次へ</a>
+                    <a class="" href="">>></a>
+                </div>
+            </div>
+        </main>
+        <?php get_sidebar(); ?>
+        <?php get_footer(); ?>
---
+++ single.php
@@ -0,0 +1,167 @@
+<?php 
+/**
+ * Template Name: single-menu
+ * Template Post Type: post,
+ */
+get_header(); ?>
+        <main class="l-main c-grid">
+            <div class="c-mainvisual c-mainvisual--another">
+                <div class="c-mainvisual__headline--another">
+                    <h1>h1 チーズバーガー</h1>
+                </div>
+                <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/mainvisual/single-sp.png?raw=true" alt="" class="c-mainvisual__sp">
+                <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/mainvisual/single--tab.png?raw=true" alt="" class="c-mainvisual__tab">
+                <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/mainvisual/single--pc.png?raw=true" alt="" class="c-mainvisual__pc">
+            </div>
+            <article class="p-article u-width--89 u-mg--tb30-lr-auto">
+                <h2>見出しh2</h2>
+                <p>
+                    Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。
+                    Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。
+                    Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。
+                    Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。
+                    Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。
+                </p>
+                <h3>見出しh3</h3>
+                <h4>見出しh4</h4>
+                <h5>見出しh5</h5>
+                <h6>見出しh6</h6>
+                <blockquote>
+                    <p>
+                        Blockquote 引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
+                        引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
+                        引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
+                        引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
+                        引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
+                        引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
+                        引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
+                        引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
+                        引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
+                        引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
+                        引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
+                        引用タグ
+                    </p>
+                    <cite>出典元：<a href="">○○○○○○○○○○○○</a></cite>
+                </blockquote>
+                <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/single/single-page__card-rectangle.png?raw=true" alt="">
+                <figure>
+                    <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/single/card.png?raw=true" alt="">
+                    <figcaption>
+                        テキストが入ります テキストが入ります テキストが入ります
+                        テキストが入ります テキストが入ります テキストが入ります
+                        テキストが入ります テキストが入ります テキストが入りま
+                    </figcaption>
+                </figure>
+                <figure>
+                    <figcaption>
+                        テキストが入ります テキストが入ります テキストが入ります
+                        テキストが入ります テキストが入ります テキストが入ります
+                        テキストが入ります テキストが入ります テキストが入ります
+                    </figcaption>
+                    <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/single/card.png?raw=true" alt="">
+                </figure>
+                <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/single/card.png?raw=true" class="u-width--89 u-img--special" alt="">
+                <div id="c-grid--wrapper">
+                    <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/single/card.png?raw=true" alt="">
+                    <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/single/card.png?raw=true" alt="">
+                    <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/single/card.png?raw=true" alt="">
+                    <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/single/card.png?raw=true" alt="">
+                    <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/single/card.png?raw=true" alt="">
+                    <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/single/card.png?raw=true" alt="">
+                    <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/single/card.png?raw=true" alt="">
+                    <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/single/card.png?raw=true" alt="">
+                    <img src="https://github.com/kamadamayo/HamburgerSite-RaiseTech--2/blob/main/images/single/card.png?raw=true" alt="">
+                </div>
+                <div class="c-list u-width--89 u-mg--tb30-lr-auto">
+                    <div class="c-list--num">
+                        <ol>
+                            <li>リストリストリスト</li>
+                            <li>リストリストリスト</li>
+                        </ol>
+                        <ol>
+                            <li>リスト2リスト2リスト2</li>
+                            <li>リスト2リスト2リスト2</li>
+                        </ol>
+                        <ol>
+                            <li>リストリストリスト</li>
+                            <li>リストリストリスト</li>
+                        </ol>
+                    </div>
+                    <div class="c-list--bul">
+                        <ul>
+                            <li>リストリストリスト</li>
+                            <li>リストリストリスト</li>
+                        </ul>
+                        <ul>
+                            <li>リスト2リスト2リスト2</li>
+                            <li>リスト2リスト2リスト2</li>
+                        </ul>
+                        <ul>
+                            <li>リストリストリスト</li>
+                            <li>リストリストリスト</li>
+                        </ul>
+                    </div>
+                </div>
+                <pre class="u-mg--tb30-lr-auto">
+                    <code>
+                        &lt;html&gt;<!--エスケープ処理-->
+                            &lt;head&gt;
+                            &lt;/head&gt;
+                            &lt;body&gt;
+                            &lt;/body&gt;
+                        &lt;/html&gt;
+                    </code>
+                </pre>
+                <table class="u-mg--tb30-lr-auto">
+                    <tr>
+                        <td>テーブル</td>
+                        <td>テーブル</td>
+                    </tr>
+                    <tr>
+                        <td>テーブル</td>
+                        <td>テーブル</td>
+                    </tr>
+                    <tr>
+                        <td>テーブル</td>
+                        <td>テーブル</td>
+                    </tr>
+                    <tr>
+                        <td>テーブル</td>
+                        <td>テーブル</td>
+                    </tr>
+                </table>
+                <button class="c-btn c-btn--detail u-width--89">ボタン</button>
+                <div class="p-article__text--bold">
+                    <p>
+                        boldboldboldboldboldboldbold
+                    </p>
+                </div>
+            </article>
+            <!--ここから検討-->
+            <div class="p-pagination">
+                <div class="u-visibility__tab">
+                    <p>page</p>
+                    <p>1</p>
+                    <p>10</p>
+                </div>
+                <div class="p-pagination__prev">
+                    <a class="" href=""><<</a>
+                    <a class="u-visibility__sp" href="">前へ</a>
+                </div>
+                <a href="" class="p-pagination__number u-visibility__tab">1</a>
+                <a href="" class="p-pagination__number u-visibility__tab">2</a>
+                <a href="" class="p-pagination__number u-visibility__tab">3</a>
+                <a href="" class="p-pagination__number u-visibility__tab">4</a>
+                <a href="" class="p-pagination__number u-visibility__tab">5</a>
+                <a href="" class="p-pagination__number u-visibility__tab">6</a>
+                <a href="" class="p-pagination__number u-visibility__tab">7</a>
+                <a href="" class="p-pagination__number u-visibility__tab">8</a>
+                <a href="" class="p-pagination__number u-visibility__tab">9</a>
+                <div class="p-pagination__next">
+                    <a class="u-visibility__sp" href="">次へ</a>
+                    <a class="" href="">>></a>
+                </div>
+            </div>
+        </main>
+        <?php get_sidebar(); ?>
+        <?php get_footer(); ?>
